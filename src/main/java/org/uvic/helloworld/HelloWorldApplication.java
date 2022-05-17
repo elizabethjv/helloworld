4ff7f7fe334470c5b37b9f99c85bc15acc6ebd5b
Sanjay Dutt <sanjaydutt@uvic.ca>
2022-05-17 12:52:00 -0700

    Commit Project

---
+++ src/main/java/org/uvic/helloworld/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package org.uvic.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
---
+++ src/main/java/org/uvic/helloworld/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.uvic.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello! You are awesome.";
+    }
+}
